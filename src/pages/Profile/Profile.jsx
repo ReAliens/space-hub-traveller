3115f624eaed56a6ee5d429b63ce1bfb3b09d4f8
Rayhan <nofussdufus@gmail.com>
2022-01-25 11:20:43 +0000

    Start render reserved rockets in profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,7 +1,21 @@
-const Profile = () => (
-  <div>
-    <h1>Hello from my Profile page</h1>
-  </div>
-);
+import { useSelector } from 'react-redux';
+
+const Profile = () => {
+  let { rockets } = useSelector((state) => state.rocketReducer);
+  rockets = rockets.filter((rocket) => rocket.reserved);
+  console.log(rockets);
+  return (
+    <div className="flex flex-col">
+      <h1>Hello from my Profile page</h1>
+      <ul className="text-lg bg-slate-400 font-bold">
+        {rockets.map((rocket) => (
+          <li key={rocket.id}>
+            {rocket.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Profile;

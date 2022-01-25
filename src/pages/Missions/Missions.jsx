bfd6ce0f6e44086dc70f711a26b459cc2833b706
Ahmed <ahmedreda152@gmail.com>
2022-01-25 09:07:36 +0200

    add spinner component

--- src/pages/Missions/Missions.jsx
+++ src/pages/Missions/Missions.jsx
@@ -1,17 +1,26 @@
 import { useSelector } from 'react-redux';
+import Spinner from '../../Components/Spinner/Spinner';
 import TableHeader from '../../Components/TableHeader/TableHeader';
 import TableRow from '../../Components/TableRow/TableRow';
 
 const Missions = () => {
-  const missions = useSelector((state) => state.missionReducer.data);
+  const missions = useSelector((state) => state.missionReducer);
   return (
     <div className="px-10">
-      <TableHeader />
-      <div className="grid grid-flow-row pb-8">
-        {missions.map((mission, index) => (
-          <TableRow key={mission.id} data={mission} index={index} />
-        ))}
-      </div>
+      {missions.loading ? (
+        <div className="h-screen overflow-hidden w-full flex items-center justify-center">
+          <Spinner />
+        </div>
+      ) : (
+        <>
+          <TableHeader />
+          <div className="grid grid-flow-row pb-8">
+            {missions.data.map((mission, index) => (
+              <TableRow key={mission.id} data={mission} index={index} />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
